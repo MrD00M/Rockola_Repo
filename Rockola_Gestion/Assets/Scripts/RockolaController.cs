9c3244f1730522fa3fda7647ef5ff3ecfa92a290
MrD00M <camilo.villegas@upb.edu.co>
2018-01-31 12:27:18 -0500

    RockolaController.cs I
    La Rockola ahora tiene un Audiosource y 5 posibles canciones para tocar.

---
+++ Rockola_Gestion/Assets/Scripts/RockolaController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class RockolaController : MonoBehaviour {
+
+	[SerializeField]
+	private AudioClip[] alternateSound;
+
+
+
+	private AudioSource myAudioSource;
+
+	// Use this for initialization
+	void Start () {
+
+		myAudioSource = GetComponent<AudioSource> ();
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
