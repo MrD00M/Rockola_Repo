a0b52d6deb09b7475ed812c1e7ab3644f915f3e6
MrD00M <camilo.villegas@upb.edu.co>
2018-01-31 18:24:24 -0500

    TextController.cs II
    Se ha agregado el metodo "ChangeLeft" que permite cambiar el texto al
    titulo de la cancion actual.

--- Rockola_Gestion/Assets/Scripts/TextController.cs
+++ Rockola_Gestion/Assets/Scripts/TextController.cs
@@ -13,7 +13,6 @@ public class TextController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		//alternateText = GetComponent<Text> ();
 	}
 
 	public void ChangeRight() {
@@ -21,13 +20,38 @@ public class TextController : MonoBehaviour {
 
 		if (textIndex >= 5){
 			textIndex = 0;
-			//myAudioSource.clip = alternateSound [currentSoundIndex];
 		}
 		switch (textIndex)
 		{
 		case 0:
 			alternateText.text = "Brazil anthem";
-			//alternateText.text = "Brazil anthem";
+			Debug.Log ("si funciono");
+			break;
+		case 1:
+			alternateText.text = "Israel anthem";
+			break;
+		case 2:
+			alternateText.text = "Peru anthem";
+			break;
+		case 3:
+			alternateText.text = "Turkey anthem";
+			break;
+		case 4:
+			alternateText.text = "USSR anthem";
+			break;
+		}
+	}
+
+	public void ChangeLeft() {
+		textIndex -= 1;
+
+		if (textIndex <= -1){
+			textIndex = 5;
+		}
+		switch (textIndex)
+		{
+		case 0:
+			alternateText.text = "Brazil anthem";
 			Debug.Log ("si funciono");
 			break;
 		case 1:
