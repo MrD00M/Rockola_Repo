ff8f7a1e1590003f75b43ef225d406a209b1f759
MrD00M <camilo.villegas@upb.edu.co>
2018-01-31 20:43:03 -0500

    ImageController.cs II
    Se ha agregado el metodo "ChangeRight" que activa la imagen de la
    cancion sonando actualmente.

--- Rockola_Gestion/Assets/Scripts/ImageController.cs
+++ Rockola_Gestion/Assets/Scripts/ImageController.cs
@@ -5,17 +5,28 @@ using UnityEngine.UI;
 
 public class ImageController : MonoBehaviour {
 
+	//Se toma la referencia de las imagenes y se define el index
 	[SerializeField]
 	private RawImage[] alternateImage;
+
 	public int imageIndex = -1;
 
-	// Use this for initialization
 	void Start () {
-		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void ChangeRight() {
+		//Se cambia la imagen a la derecha
+		imageIndex += 1;
+
+		if (imageIndex >= 5) {
+			imageIndex = 0;
+			alternateImage [0].enabled = false;
+			alternateImage [1].enabled = false;
+			alternateImage [2].enabled = false;
+			alternateImage [3].enabled = false;
+			alternateImage [4].enabled = false;
+		}
+		alternateImage[imageIndex].enabled = true;
 	}
+		
 }
