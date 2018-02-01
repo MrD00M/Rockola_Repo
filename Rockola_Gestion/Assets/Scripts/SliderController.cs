d73e3d52374a1cf4a7c573e795641dd33bcdffd8
MrD00M <camilo.villegas@upb.edu.co>
2018-01-31 19:10:49 -0500

    SliderController.cs I
    Se ha agregado el metodo "ChangeVolume" que permite cambiar el valor del
    volumen del audiosource a travez de un slider.

---
+++ Rockola_Gestion/Assets/Scripts/SliderController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour {
+
+	[SerializeField]
+	private Slider volumeSlider;
+
+	[SerializeField]
+	private AudioSource volumeAudio;
+
+	public void ChangeVolume(){
+		volumeAudio.volume = volumeSlider.value;
+	}
+		
+}
